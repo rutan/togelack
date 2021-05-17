d34a0c0e74a7b127efd0ee3b645f831d89f0774c
ru_shalm <ru_shalm@hazimu.com>
2021-05-18 02:35:53 +0900

    fix rubocop

--- config/initializers/gemoji.rb
+++ config/initializers/gemoji.rb
@@ -1,14 +1,12 @@
-begin
-  [
-    { target: 'thinking', aliases: ['thinking_face'] }
-  ].each do |item|
-    emoji = Emoji.find_by_alias(item[:target])
-    raise "emoji: #{item[:target]} is not defined" unless emoji
+[
+  { target: 'thinking', aliases: ['thinking_face'] }
+].each do |item|
+  emoji = Emoji.find_by_alias(item[:target])
+  raise "emoji: #{item[:target]} is not defined" unless emoji
 
-    Emoji.edit_emoji(emoji) do |char|
-      item[:aliases].each do |alias_name|
-        char.add_alias(alias_name)
-      end
+  Emoji.edit_emoji(emoji) do |char|
+    item[:aliases].each do |alias_name|
+      char.add_alias(alias_name)
     end
   end
 end
