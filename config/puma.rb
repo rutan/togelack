7b306a902331d31d1186c5045da412574cfc09d9
ru_shalm <ru_shalm@hazimu.com>
2021-05-18 01:36:07 +0900

    cleanup puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,15 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
+max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
+min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
+threads min_threads_count, max_threads_count
 
+worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
+
+port ENV.fetch('PORT', 3000)
+
+environment ENV.fetch('RAILS_ENV', 'development')
+pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
+
+workers ENV.fetch('WEB_CONCURRENCY', 2)
 preload_app!
 
-rackup DefaultRackup
-port ENV['PORT'] || 3000
-environment ENV['RAILS_ENV'] || 'development'
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-end
+plugin :tmp_restart
